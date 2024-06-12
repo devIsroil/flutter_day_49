0143ec75c1a149a400932bcef5959219e5d8b01d
Isroil <isroilkhasanov98@gmail.com>
2024-06-12 23:22:55 +0500

    last commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,16 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_lesson_49/homework/todos_and_notes/notifier/notes_notifier.dart';
-import 'package:flutter_lesson_49/homework/todos_and_notes/viewmodels/notes_view_model.dart';
-import 'package:flutter_lesson_49/homework/todos_and_notes/views/screens/password_screen.dart';
 
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'homework/todos_and_notes/models/course_model.dart';
+import 'homework/todos_and_notes/notifier/notes_notifier.dart';
 import 'homework/todos_and_notes/utils/app_constants.dart';
 import 'homework/todos_and_notes/utils/routes.dart';
-import 'homework/todos_and_notes/views/screens/course_admin_screen.dart';
+import 'homework/todos_and_notes/viewmodels/notes_view_model.dart';
 import 'homework/todos_and_notes/views/screens/course_info_screen.dart';
 import 'homework/todos_and_notes/views/screens/home_screen.dart';
-import 'homework/todos_and_notes/views/screens/last_screen.dart';
 import 'homework/todos_and_notes/views/screens/main_screen.dart';
 import 'homework/todos_and_notes/views/screens/profile_screen.dart';
 import 'homework/todos_and_notes/views/screens/results_screen.dart';
@@ -79,12 +76,34 @@ class _MyAppState extends State<MyApp> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    AppConstants().setConstants().then((_) {
+      setState(() {});
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return NoteNotifier(
       noteController: NoteController(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
+          buttonTheme: const ButtonThemeData(
+              buttonColor: Colors.blue
+          ),
+          colorSchemeSeed: Colors.blue,
+          textButtonTheme: const TextButtonThemeData(
+            style: ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(Colors.blue),
+            ),
+          ),
+          elevatedButtonTheme: const ElevatedButtonThemeData(
+            style: ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(Colors.blue),
+            ),
+          ),
           appBarTheme: AppBarTheme(
             backgroundColor: AppConstants.appColor,
           ),
@@ -102,16 +121,16 @@ class _MyAppState extends State<MyApp> {
         return CupertinoPageRoute(
           builder: (BuildContext context) => const TodoScreen(),
         );
-      case RouteNames.courseAdmin:
-        return CupertinoPageRoute(
-          builder: (BuildContext context) => CourseAdminScreen(
-            onThemeChanged: toggleThemeMode,
-            onBackgroundChanged: onBackgroundChanged,
-            onLanguageChanged: onLanguageChanged,
-            onColorChanged: onColorChanged,
-            onTextChanged: onTextChanged,
-          ),
-        );
+      // case RouteNames.admin:
+      //   return CupertinoPageRoute(
+      //     builder: (BuildContext context) => AdminPage(
+      //       onThemeChanged: toggleThemeMode,
+      //       onBackgroundChanged: onBackgroundChanged,
+      //       onLanguageChanged: onLanguageChanged,
+      //       onColorChanged: onColorChanged,
+      //       onTextChanged: onTextChanged,
+      //     ),
+      //   );
       case RouteNames.courseInfo:
         return CupertinoPageRoute(
           builder: (BuildContext context) =>
@@ -125,16 +144,16 @@ class _MyAppState extends State<MyApp> {
         return CupertinoPageRoute(
           builder: (BuildContext context) => const ProfileScreen(),
         );
-      case RouteNames.pinScreen:
-        return CupertinoPageRoute(
-          builder: (BuildContext context) => PinScreen(
-            onThemeChanged: toggleThemeMode,
-            onBackgroundChanged: onBackgroundChanged,
-            onLanguageChanged: onLanguageChanged,
-            onColorChanged: onColorChanged,
-            onTextChanged: onTextChanged,
-          ),
-        );
+      // case RouteNames.onboarding:
+      //   return CupertinoPageRoute(
+      //     builder: (BuildContext context) => Onboarding(
+      //       onThemeChanged: toggleThemeMode,
+      //       onBackgroundChanged: onBackgroundChanged,
+      //       onLanguageChanged: onLanguageChanged,
+      //       onColorChanged: onColorChanged,
+      //       onTextChanged: onTextChanged,
+      //     ),
+      //   );
       case RouteNames.resultScreen:
         return CupertinoPageRoute(
           builder: (BuildContext context) => const ResultsScreen(),
@@ -149,15 +168,15 @@ class _MyAppState extends State<MyApp> {
             onTextChanged: onTextChanged,
           ),
         );
-     // case RouteNames.lastScreen:
-        // final Map<String, dynamic> data =
-        // settings.arguments as Map<String, dynamic>;
-        // return CupertinoPageRoute(
-        //   builder: (BuildContext context) => LastScreen(
-        //     course: data['course'] as Course,
-        //     index: data['index'] as int,
-        //   ),
-        // );
+      // case RouteNames.youTubeVideo:
+      //   final Map<String, dynamic> data =
+      //   settings.arguments as Map<String, dynamic>;
+      //   return CupertinoPageRoute(
+      //     builder: (BuildContext context) => YouTubeVideoScreen(
+      //       course: data['course'] as Course,
+      //       index: data['index'] as int,
+      //     ),
+      //   );
       case RouteNames.homeScreen:
         return CupertinoPageRoute(
           builder: (BuildContext context) => HomeScreen(
@@ -168,10 +187,21 @@ class _MyAppState extends State<MyApp> {
             onTextChanged: onTextChanged,
           ),
         );
-
+      // case RouteNames.register:
+      //   return CupertinoPageRoute(
+      //     builder: (BuildContext context) => const RegisterScreen(),
+      //   );
+      // case RouteNames.login:
+      //   return CupertinoPageRoute(
+      //     builder: (BuildContext context) => const LoginScreen(),
+      //   );
+      // case RouteNames.resetPassword:
+      //   return CupertinoPageRoute(
+      //     builder: (BuildContext context) => const ResetPasswordScreen(),
+      //   );
       default:
         return CupertinoPageRoute(
-          builder: (BuildContext context) => PinScreen(
+          builder: (BuildContext context) => HomeScreen(
             onThemeChanged: toggleThemeMode,
             onBackgroundChanged: onBackgroundChanged,
             onLanguageChanged: onLanguageChanged,
@@ -182,3 +212,189 @@ class _MyAppState extends State<MyApp> {
     }
   }
 }
+//
+//
+// import 'dart:convert';
+//
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_lesson_49/homework/todos_and_notes/notifier/notes_notifier.dart';
+// import 'package:flutter_lesson_49/homework/todos_and_notes/viewmodels/notes_view_model.dart';
+// import 'package:flutter_lesson_49/homework/todos_and_notes/views/screens/password_screen.dart';
+//
+// import 'package:shared_preferences/shared_preferences.dart';
+//
+// import 'homework/todos_and_notes/models/course_model.dart';
+// import 'homework/todos_and_notes/utils/app_constants.dart';
+// import 'homework/todos_and_notes/utils/routes.dart';
+// import 'homework/todos_and_notes/views/screens/course_admin_screen.dart';
+// import 'homework/todos_and_notes/views/screens/course_info_screen.dart';
+// import 'homework/todos_and_notes/views/screens/home_screen.dart';
+// import 'homework/todos_and_notes/views/screens/last_screen.dart';
+// import 'homework/todos_and_notes/views/screens/main_screen.dart';
+// import 'homework/todos_and_notes/views/screens/profile_screen.dart';
+// import 'homework/todos_and_notes/views/screens/results_screen.dart';
+// import 'homework/todos_and_notes/views/screens/settings_screen.dart';
+// import 'homework/todos_and_notes/views/screens/todo_screen.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//   void toggleThemeMode(bool value) async {
+//     AppConstants.themeMode = value ? ThemeMode.dark : ThemeMode.light;
+//     SharedPreferences preferences = await SharedPreferences.getInstance();
+//     preferences.setString('isDark', '$value');
+//     setState(() {});
+//   }
+//
+//   void onBackgroundChanged(String imageUrl) async {
+//     AppConstants.imageUrl = imageUrl;
+//     SharedPreferences preferences = await SharedPreferences.getInstance();
+//     preferences.setString('image', imageUrl);
+//     setState(() {});
+//   }
+//
+//   void onLanguageChanged(String language) async {
+//     AppConstants.language = language;
+//     SharedPreferences preferences = await SharedPreferences.getInstance();
+//     preferences.setString('language', language);
+//     setState(() {});
+//   }
+//
+//   void onColorChanged(Color color) async {
+//     AppConstants.appColor = color;
+//     SharedPreferences preferences = await SharedPreferences.getInstance();
+//     preferences.setInt('app-color', color.value);
+//     setState(() {});
+//   }
+//
+//   void onTextChanged(Color color, double size) async {
+//     AppConstants.textSize = size;
+//     AppConstants.textColor = color;
+//     SharedPreferences preferences = await SharedPreferences.getInstance();
+//
+//     preferences.setString(
+//       'text-val',
+//       jsonEncode(
+//         {
+//           'text-size': size,
+//           'text-color': color.value,
+//         },
+//       ),
+//     );
+//     setState(() {});
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return NoteNotifier(
+//       noteController: NoteController(),
+//       child: MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         theme: ThemeData(
+//           appBarTheme: AppBarTheme(
+//             backgroundColor: AppConstants.appColor,
+//           ),
+//         ),
+//         darkTheme: ThemeData.dark(),
+//         themeMode: AppConstants.themeMode,
+//         onGenerateRoute: _generateRoute,
+//       ),
+//     );
+//   }
+//
+//   Route<dynamic> _generateRoute(RouteSettings settings) {
+//     switch (settings.name) {
+//       case RouteNames.todoScreen:
+//         return CupertinoPageRoute(
+//           builder: (BuildContext context) => const TodoScreen(),
+//         );
+//       case RouteNames.courseAdmin:
+//         return CupertinoPageRoute(
+//           builder: (BuildContext context) => CourseAdminScreen(
+//             onThemeChanged: toggleThemeMode,
+//             onBackgroundChanged: onBackgroundChanged,
+//             onLanguageChanged: onLanguageChanged,
+//             onColorChanged: onColorChanged,
+//             onTextChanged: onTextChanged,
+//           ),
+//         );
+//       case RouteNames.courseInfo:
+//         return CupertinoPageRoute(
+//           builder: (BuildContext context) =>
+//               CourseInfoScreen(course: settings.arguments as Course),
+//         );
+//       case RouteNames.mainScreen:
+//         return CupertinoPageRoute(
+//           builder: (BuildContext context) => const MainScreen(),
+//         );
+//       case RouteNames.profileScreen:
+//         return CupertinoPageRoute(
+//           builder: (BuildContext context) => const ProfileScreen(),
+//         );
+//       case RouteNames.pinScreen:
+//         return CupertinoPageRoute(
+//           builder: (BuildContext context) => PinScreen(
+//             onThemeChanged: toggleThemeMode,
+//             onBackgroundChanged: onBackgroundChanged,
+//             onLanguageChanged: onLanguageChanged,
+//             onColorChanged: onColorChanged,
+//             onTextChanged: onTextChanged,
+//           ),
+//         );
+//       case RouteNames.resultScreen:
+//         return CupertinoPageRoute(
+//           builder: (BuildContext context) => const ResultsScreen(),
+//         );
+//       case RouteNames.settingsScreen:
+//         return CupertinoPageRoute(
+//           builder: (BuildContext context) => SettingsScreen(
+//             onThemeChanged: toggleThemeMode,
+//             onBackgroundChanged: onBackgroundChanged,
+//             onLanguageChanged: onLanguageChanged,
+//             onColorChanged: onColorChanged,
+//             onTextChanged: onTextChanged,
+//           ),
+//         );
+//     // case RouteNames.lastScreen:
+//     // final Map<String, dynamic> data =
+//     // settings.arguments as Map<String, dynamic>;
+//     // return CupertinoPageRoute(
+//     //   builder: (BuildContext context) => LastScreen(
+//     //     course: data['course'] as Course,
+//     //     index: data['index'] as int,
+//     //   ),
+//     // );
+//       case RouteNames.homeScreen:
+//         return CupertinoPageRoute(
+//           builder: (BuildContext context) => HomeScreen(
+//             onThemeChanged: toggleThemeMode,
+//             onBackgroundChanged: onBackgroundChanged,
+//             onLanguageChanged: onLanguageChanged,
+//             onColorChanged: onColorChanged,
+//             onTextChanged: onTextChanged,
+//           ),
+//         );
+//
+//       default:
+//         return CupertinoPageRoute(
+//           builder: (BuildContext context) => PinScreen(
+//             onThemeChanged: toggleThemeMode,
+//             onBackgroundChanged: onBackgroundChanged,
+//             onLanguageChanged: onLanguageChanged,
+//             onColorChanged: onColorChanged,
+//             onTextChanged: onTextChanged,
+//           ),
+//         );
+//     }
+//   }
+// }
